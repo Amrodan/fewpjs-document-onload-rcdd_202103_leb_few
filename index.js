00a9f735c15323970a318dc1ceba76fc657d93a5
Amro dandashli <amrdandashli@gmail.com>
2021-04-15 02:05:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+let a=  document.getElementById("text").innerText="This is really cool!"
+});
